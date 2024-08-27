0915cd5b74efbac8ccf244e1af9c10471040b2d7
Mattia Migliore <mattiamigliore.com@gmail.com>
2024-08-27 18:55:55 +0200

    wip -> add jwt to websocket connection

--- src/notification.ts
+++ src/notification.ts
@@ -1,3 +1,10 @@
 import { Client } from "./core"
+import { ApiKeyAuthorized, Maybe } from "./types"
 
-export class Notification extends Client {}
+export class Notification extends Client {
+  private _socket: Maybe<WebSocket> = null
+
+  constructor(config: ApiKeyAuthorized) {
+    super(config.devMode)
+  }
+}
