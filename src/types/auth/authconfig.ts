ebf6d99b85532bf3e973c70ee76c8f7add5edbc9
Mattia Migliore <mattiamigliore.com@gmail.com>
2024-10-18 01:04:36 +0200

    wip -> removed imports

--- src/types/auth/authconfig.ts
+++ src/types/auth/authconfig.ts
@@ -1,10 +1,5 @@
 import { PrivyClientConfig } from "@privy-io/react-auth"
 import { DexieStorage } from "../../core/app"
-import { Oracle } from "@src/oracle"
-import { Order } from "@src/order"
-import { Proposal } from "@src/proposal"
-import { Chat } from "@src/chat"
-import { Notification } from "@src/notification"
 
 /**
  * Represents the configuration for authentication.
