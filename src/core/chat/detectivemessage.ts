9c10b85d4bae9087a42ed88f853d6105116a4568
Mattia Migliore <mattiamigliore.com@gmail.com>
2024-11-01 18:21:33 +0100

    wip -> stopScan removed from clear

--- src/core/chat/detectivemessage.ts
+++ src/core/chat/detectivemessage.ts
@@ -311,11 +311,14 @@ export class DetectiveMessage {
     if (!DetectiveMessage._detectiveMessageCanRun) return
     if (this._detectiveMessageTimeout)
       clearTimeout(this._detectiveMessageTimeout)
+
+    console.warn(
+      "Detective message, scan is disabled. If you wanna use it and this action was unintentional, this is a reminder to call again the scan() method in order to restore it."
+    )
   }
 
   clear() {
     this.unobserve()
-    this.stopScan()
     DetectiveMessage._storage.truncate("detectivemessagecollector")
     DetectiveMessage._storage.truncate("detectivemessagequeue")
   }
@@ -397,21 +400,4 @@ export class DetectiveMessage {
       current.order > highest.order ? current : highest
     )
   }
-
-  /**
-   * RULES detective message
-   */
-
-  //table on IndexedDB should contain a primary key made by the conversationId, userId, messageId, version
-  //structure of the table should be conversationId, userId, messageId, version, createdAt
-
-  //every time a message comes, insert a data with the version and the id of the message inside the db or local storage for each conversation
-  //it takes into consideration the last message validated, in order to check with the server the messages with a version number greater than the version registered
-  //it saves the messages under a form of a list of version number
-  //[9] - [10] - [13] - [14] - [15] | {8} <- this is the last message validated ?? maybe this part is useless because we store simply the list of the messages
-  //[11] and [12] are missing. Ok, let's check if the server has these messages
-  //it starts to communicate with the server only if the client explicitly is on the conversation
-  //if it has some results already in the queue list, it sends them on the server once the client starts to observe a conversation
-  //it activates a timeout and cyclely communicate with the server only if the client is explicitly on the conversation
-  //once it gets the desired messages from the server, it store them on the local db
 }
